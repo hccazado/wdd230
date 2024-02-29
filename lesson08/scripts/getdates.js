a91381bf8a431f97e2d444b5933f0e00a2201167
Heitor Cazado <heitorcazado@gmail.com>
2024-02-29 12:00:43 -0300

    W8 fixing form activity directory

--- lesson08/scripts/getdates.js
+++
@@ -1,6 +0,0 @@
-const options = {"day":"numeric", "month":"short", "year":"numeric"};
-let currentDate = new Date().toLocaleDateString("en-US", options);
-
-document.querySelector("#ct-year").innerHTML = new Date().toLocaleDateString("en-US", {"year":"numeric"});
-
-document.querySelector("#lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,12 @@
 const options = {"day":"numeric", "month":"short", "year":"numeric"};
 let currentDate = new Date().toLocaleDateString("en-US", options);
 
-document.querySelector("#ct-date").innerHTML = currentDate;
+const ctdate = document.querySelector("#ct-date");
+
+if (ctdate){
+    ctdate.innerHTML = currentDate;
+}
+
 document.querySelector("#ct-year").innerHTML = new Date().toLocaleDateString("en-US", {"year":"numeric"});
 
 document.querySelector("#lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
