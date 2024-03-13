55730905faec377dae437a7871c4232f8aa87988
Heitor Cazado <heitorcazado@gmail.com>
2024-03-13 12:00:33 -0300

    chamber project finished banner and script

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -2,6 +2,18 @@ const btn = document.querySelector("#banner-btn");
 const banner = document.querySelector("#hp-banner");
 
 btn.addEventListener("click", () =>{
-    console.log("click")
-    banner.style.display = "none";
-})
+    banner.classList.remove("show");
+    banner.classList.add("hide");
+});
+
+function displayBanner(){
+    const currentDate = new Date(Date.now());
+    const wDay = currentDate.getDay(); // Sunday - Saturday : 0 - 6
+    const hour = currentDate.getHours();
+    if(wDay == 1 || wDay == 2 || wDay == 3 && hour < 19){
+        banner.classList.remove("hide");
+        banner.classList.add("show");
+    }
+}
+
+displayBanner();
