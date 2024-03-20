5c1897ba19fc94875f85e413c9ca725bf062796e
Heitor Cazado <heitorcazado@gmail.com>
2024-03-20 14:32:55 -0300

    chamber project updt style and added waypoint

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,7 +1,18 @@
 const hmbBtn = document.querySelector("#hmbbtn");
 const navBar = document.querySelector(".navlist");
+const navLinks = document.querySelectorAll(".nav-link");
 
 hmbBtn.addEventListener('click', ()=>{
     navBar.classList.toggle('show');
     hmbBtn.classList.toggle('show');
 });
+
+function highlightLink(){
+    navLinks.forEach(link=>{
+        if(document.location.pathname == link.pathname){
+            link.classList.toggle("active");
+        }
+    });
+}
+
+highlightLink();
