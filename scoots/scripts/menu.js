461631339a265e2beb50373bb79c01aeef11488f
Heitor Cazado <heitorcazado@gmail.com>
2024-04-08 17:50:26 -0300

    scots base style completed checked html and css

--- scoots/scripts/menu.js
+++ scoots/scripts/menu.js
@@ -1,6 +1,17 @@
 const btn = document.querySelector("#menuBtn");
 const menu = document.querySelector("#navbar");
+const navLinks = document.querySelectorAll(".nav-link");
 
 btn.addEventListener("click", ()=>{
     menu.classList.toggle("show");
-})
+});
+
+function highlightLink(){
+    navLinks.forEach(link=>{
+        if(document.location.pathname == link.pathname){
+            link.classList.toggle("active");
+        }
+    });
+}
+
+highlightLink();
